5e700beaf7d43689ecd107789930f5f58459e74b
mdjameel04 <jameeltony67@gmail.com>
2025-12-20 00:48:27 +0530

    inital commit

--- app/create/_components/LogoTitle.jsx
+++ app/create/_components/LogoTitle.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import HeadingDescription from './HeadingDescription'
 import Lookup from '@/app/_data/Lookup'
+import { useSearchParams } from 'next/navigation'
 
-const LogoTitle = () => {
+const LogoTitle = (onHandleIputChange) => {
+  const searchParmas = useSearchParams();
+  const [title, setTitle] = useState(searchParmas?.get('title'));
   return (
     <div className='my-10'>
         <HeadingDescription
         title={Lookup.LogoTitle}
         description={Lookup.LogoTitleDesc}
         />
+        <input placeholder={Lookup.InputTitlePlaceholder}
+        className='p-4 border w-full rounded-lg mt-5'
+        defaultValue={title}
+        onChange={(e)=>onHandleIputChange(e.target.value)}
+        />
+        
     </div>
   )
 }
--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import LogoTitle from "./_components/LogoTitle"
+import { Button } from '@/components/ui/button'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 
 const CreatePage = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState();
+  const onHandleIputChange = (field, value)=>{
+  setFormData(prev=>({
+    ...prev,
+    [field]: value
+  }))
+  }
   return (
     <div className='mt-20 p-10 border rounded-xl 2xl:mx-72'>
-       <LogoTitle/>
+      {step==1? <LogoTitle onHandleIputChange={(v)=>onHandleIputChange("title",v)} />:
+      null
+      }
 
+   <div className='flex items-center justify-between mt-10'>
+   {step!=1 && <Button variant="outline" onClick={()=>setStep(step-1)}> <ArrowLeft/> Pervious </Button>}
+    <Button onClick={()=>setStep(step+1)}> <ArrowRight/> next </Button>
+   </div>
     </div>
   )
 }
